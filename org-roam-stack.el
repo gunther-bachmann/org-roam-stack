8fb56dadb72e43b7d9dc6f36592b2b69a69fbf85
Gunther Bachmann <gunther.bachmann@itemis.de>
2020-11-23 20:22:09 +0100

    prepare org-roam-stack to be used as package, fix some bugs

--- org-roam-stack.el
+++ org-roam-stack.el
@@ -1,4 +1,47 @@
-;; scratchpad
+;;; org-roam-stack.el --- organize org roam in stack -*- lexical-binding:t -*-
+
+;; Copyright (C) 2019-2020  Free Software Foundation, Inc.
+
+;; Author: Gunther Bachmann <gunther.bachmann@web.de>
+;; Maintainer: gunther.bachmann@web.de
+;; Package: org-roam-stack
+;; Homepage: https://github.com/gunther-bachmann/org-roam-slack
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "27"))
+;; Keywords: internal
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; https://github.com/gunther-bachmann/org-roam-stack
+
+;;; Code:
+(eval-when-compile
+   (require 'cl))
+(require 'dash)
+(require 's)
+
+(require 'windmove)
+
+(require 'ol)
+(require 'org-protocol)
+(require 'notdeft)
+
+(defgroup org-roam-stack nil
+  "organize org roam cards in a stack"
+  :group 'org-roam)
+
 (defvar org-roam-stack--stack-height (window-height (get-buffer-window))
   "height of the stack used for balance window calculations")
 
@@ -10,6 +53,22 @@ if by some commands the list gets out of sync, org-roam-stack--restore-stack can
 
 (defvar org-roam-stack--buffer-open-resize-strategy 'maximize
   "either 'maximize or 'balance")
+
+(defvar org-roam-stack--local-keybindings
+  '(( "C-x C-k"         . org-roam-stack--remove-current-buffer-from-stack)
+    ( "C-x k"           . org-roam-stack--remove-current-buffer-from-stack)
+    ( "<S-s-return>"    . org-roam-stack--maximize-current-buffer)
+    ( "<C-s-return>"    . org-roam-stack--maximize-current-buffer)
+    ( "<S-s-left>"      . org-roam-stack--void)   ;; unbind the given merge behaviour
+    ( "<S-s-right>"     . org-roam-stack--void) ;; unbind the given merge behaviour
+    ( "<S-s-up>"        . org-roam-stack--merge-current-with-above)
+    ( "<S-s-backspace>" . org-roam-stack--balance-stack)
+    ( "<S-s-down>"      . org-roam-stack--merge-current-with-below)
+    ( "<M-s-up>"        . org-roam-stack--move-buffer-up)
+    ( "<M-s-down>"      . org-roam-stack--move-buffer-down)))
+
+(defvar org-roam-stack--focused t
+  "stay focused, dim other cards in the stack")
 
 (defun org-roam-stack--execute-buffer-open-resize-strategy ()
   (case org-roam-stack--buffer-open-resize-strategy
@@ -29,9 +88,9 @@ if by some commands the list gets out of sync, org-roam-stack--restore-stack can
   (when-let* ((idx (-elem-index existing-buffer org-roam-stack--buffer-list)))
     (setq org-roam-stack--buffer-list (-insert-at idx inserted-buffer org-roam-stack--buffer-list))))
 
-(defun org-roam-stack--insert-buffer-before-existing (buffer insert)
-  (when-let* ((idx (-elem-index buffer org-roam-stack--buffer-list)))
-    (setq org-roam-stack--buffer-list (-insert-at (+1 idx) insert org-roam-stack--buffer-list))))
+(defun org-roam-stack--insert-buffer-before-existing (existing-buffer inserted-buffer)
+  (when-let* ((idx (-elem-index existing-buffer org-roam-stack--buffer-list)))
+    (setq org-roam-stack--buffer-list (-insert-at (1+ idx) inserted-buffer org-roam-stack--buffer-list))))
 
 (defun org-roam-stack--exchange-buffers-in-list (idx-a idx-b mylist)
   "exchange elements in list at position a with position b
@@ -55,7 +114,7 @@ idx-a < idx-b!"
     (let ((buffer (current-buffer)))
       (when (eq dir 'below)
         (other-window 1))
-      (org-roam-stack--openfile roam-file)
+      (org-roam-stack--open-file roam-file)
       (if (eq dir 'below)
           (org-roam-stack--insert-buffer-after-existing buffer (current-buffer))
         (org-roam-stack--insert-buffer-before-existing buffer (current-buffer)))
@@ -127,28 +186,13 @@ idx-a < idx-b!"
     (setq org-roam-stack--buffer-list (org-roam-stack--exchange-buffers-in-list c-buffer-idx buffer-up-idx org-roam-stack--buffer-list))
     (org-roam-stack--execute-buffer-open-resize-strategy)))
 
-(defvar org-roam-stack--local-keybindings
-  '(( "C-x C-k"         . org-roam-stack--remove-current-buffer-from-stack)
-    ( "C-x k"           . org-roam-stack--remove-current-buffer-from-stack)
-    ( "<S-s-return>"    . org-roam-stack--maximize-current-buffer)
-    ( "<C-s-return>"    . org-roam-stack--maximize-current-buffer)
-    ( "<S-s-left>"      . org-roam-stack--void)   ;; unbind the given merge behaviour
-    ( "<S-s-right>"     . org-roam-stack--void) ;; unbind the given merge behaviour
-    ( "<S-s-up>"        . org-roam-stack--merge-current-with-above)
-    ( "<S-s-backspace>" . org-roam-stack--balance-stack)
-    ( "<S-s-down>"      . org-roam-stack--merge-current-with-below)
-    ( "<M-s-up>"        . org-roam-stack--move-buffer-up)
-    ( "<M-s-down>"      . org-roam-stack--move-buffer-down)))
-
-(defvar org-roam-stack--focused t
-  "stay focused, dim other cards in the stack")
-
 (defun org-roam-stack--buffer-change-hook ()
   (when (and org-roam-stack--focused (org-roam-stack--buffer-in-stack-p (current-buffer)))
     (org-roam-stack--dim-other-buffers)
     (org-roam-stack--undim-buffer (current-buffer))))
 
 (defun org-roam-stack--register-local-keybindings ()
+  (use-local-map (copy-keymap (current-local-map)))
   (--each org-roam-stack--local-keybindings
     (local-set-key (kbd (car it)) (cdr it))))
 
@@ -169,17 +213,35 @@ idx-a < idx-b!"
 (defun org-roam-stack--report ()
   (message (s-join ", " (--map (buffer-name it) org-roam-stack--buffer-list))))
 
+(defun org-roam-stack--remove-buffer-from-view-and-stack (buffer)
+  "kill the buffer and remove it from view and stack if kill is successful return t, return nil otherwise"
+  (when-let ((idx-before (-elem-index buffer org-roam-stack--buffer-list)))
+    (org-roam-stack--remove-buffer-from-list buffer)
+    (let ((killed (kill-buffer buffer)))
+      (if killed
+          (progn
+            (delete-window (get-buffer-window buffer))
+            t)
+        (setq org-roam-stack--buffer-list (-insert-at idx-before buffer org-roam-stack--buffer-list))
+        nil))))
+
 (defun org-roam-stack--merge-current-with-above ()
   (interactive)
-  (org-roam-stack--remove-buffer-from-list (org-roam-stack--get-buffer-above-existing (current-buffer)))
-  (if-let (upper-window (windmove-find-other-window 'up))
-      (delete-window upper-window)))
+  (let ((buffer (org-roam-stack--get-buffer-above-existing (current-buffer))))
+    (if (buffer-modified-p buffer)
+        (message (format "cannot remove modified card: %s" (buffer-name buffer)))
+      (if-let (upper-window (windmove-find-other-window 'up))
+          (when (equal upper-window (get-buffer-window buffer))
+            (org-roam-stack--remove-buffer-from-view-and-stack buffer))))))
 
 (defun org-roam-stack--merge-current-with-below ()
   (interactive)
-  (org-roam-stack--remove-buffer-from-list (org-roam-stack--get-buffer-below-existing (current-buffer)))
-  (if-let (lower-window (windmove-find-other-window 'down))
-      (delete-window lower-window)))
+  (let ((buffer (org-roam-stack--get-buffer-below-existing (current-buffer))))
+    (if (buffer-modified-p buffer)
+        (message (format "cannot remove modified card: %s" (buffer-name buffer)))
+      (if-let (lower-window (windmove-find-other-window 'down))
+          (when (equal lower-window (get-buffer-window buffer))
+            (org-roam-stack--remove-buffer-from-view-and-stack buffer))))))
 
 (defun org-roam-stack--void ()
   "do nothing"
@@ -204,27 +266,24 @@ idx-a < idx-b!"
   (when buffer
     (setq org-roam-stack--buffer-list (remove buffer org-roam-stack--buffer-list))))
 
-(defun org-roam-stack--remove-current-buffer-from-stack (askp)
+(defun org-roam-stack--remove-current-buffer-from-stack (_)
   "remove current buffer from stack, making space for others"
   (interactive "P")
-  (let ((buffer (current-buffer)))
-    (unless (org-roam-stack--buffer-not-in-stack-p buffer)
-      (org-roam-stack--remove-buffer-from-list buffer)
-      (ignore-errors (delete-window)))))
+  (org-roam-stack--remove-buffer-from-view-and-stack (current-buffer)))
 
 (defun org-roam-stack--maximize-current-buffer ()
   "maximize current buffer, reduce all other stack windows to minimum"
   (interactive)
   (ignore-errors
     (--dotimes 2
-      (when (< 1 (list-length org-roam-stack--buffer-list))
+      (when (< 1 (cl-list-length org-roam-stack--buffer-list))
         (enlarge-window org-roam-stack--stack-height)))))
 
 (defun org-roam-stack--balance-stack ()
   "balance (height of) all buffers in the stack"
   (interactive)
   (--dotimes 2
-    (let ((card-window-height (/ (- org-roam-stack--stack-height (list-length org-roam-stack--buffer-list)) (list-length org-roam-stack--buffer-list))))
+    (let ((card-window-height (/ (- org-roam-stack--stack-height (cl-list-length org-roam-stack--buffer-list)) (cl-list-length org-roam-stack--buffer-list))))
       (--each (-butlast (reverse org-roam-stack--buffer-list))
         (let* ((its-window (get-buffer-window it))
                (lines (window-height its-window))
@@ -242,9 +301,9 @@ idx-a < idx-b!"
 
 (defun org-roam-stack--dim-buffer (buffer)
   (with-current-buffer buffer
-    (setq focus-overlay (make-overlay (point-min) (point-max)))
-    (overlay-put focus-overlay 'face 'font-lock-comment-face)
-    (overlay-put focus-overlay 'org-roam-stack-dim t)))
+    (let ((focus-overlay (make-overlay (point-min) (point-max))))
+      (overlay-put focus-overlay 'face 'font-lock-comment-face)
+      (overlay-put focus-overlay 'org-roam-stack-dim t))))
 
 (defun org-roam-stack--undim-buffer (buffer)
   (with-current-buffer buffer
@@ -267,34 +326,17 @@ idx-a < idx-b!"
   (setq org-protocol-protocol-alist (--remove (string-equal "roam-file" (plist-get (cdr it) :protocol)) org-protocol-protocol-alist))
   (push '("org-roam-file"  :protocol "roam-file"   :function org-roam-stack--protocol-open-file) org-protocol-protocol-alist))
 
+(defun org-roam-stack--register-open-file-protocol-advice (&rest _)
+  (org-roam-stack--register-open-file-protocol))
+
 (defun org-roam-stack--unregister-open-file-protocol ()
   (setq org-protocol-protocol-alist (--remove (string-equal "roam-file" (plist-get (cdr it) :protocol)) org-protocol-protocol-alist))
   (push '("org-roam-file"  :protocol "roam-file"   :function org-roam-protocol-open-file) org-protocol-protocol-alist))
 
-(defun org-roam-stack--notdeft-find-file-advice (orig-fun &rest args)
+(defun org-roam-stack--notdeft-find-file-advice (_ &rest args)
   (message "called")
   (prog1 (org-roam-stack--open (car args))
     (notdeft-note-mode 1)))
-
-;; -------------------------------------------------------------------------------- register hooks etc
-(org-roam-stack--register-open-file-protocol)
-;; (org-roam-stack--unregister-open-file-protocol)
-
-(add-hook 'buffer-list-update-hook #'org-roam-stack--buffer-change-hook)
-;; (remove-hook 'buffer-list-update-hook #'org-roam-stack--buffer-change-hook)
-
-;; overwrite notdef-find-file
-(require 'notdeft)
-(advice-add 'notdeft-find-file :around 'org-roam-stack--notdeft-find-file-advice)
-;; (advice-remove 'notdeft-find-file 'org-roam-stack--notdeft-find-file-advice)
-
-;; (defun notdeft-find-file (file)
-;;   "Edit NotDeft note FILE.
-;; Enable NotDeft note mode for the buffer for editing the file.
-;; Called interactively, query for the FILE using the minibuffer."
-;;   (interactive "FFind NotDeft file: ")
-;;   (prog1 (org-roam-stack--open file) ;; <-- cannot be customized, maybe use advice for notdeft-find-file
-;;     (notdeft-note-mode 1)))
 
 ;; experimental code
 (when nil
@@ -314,3 +356,25 @@ idx-a < idx-b!"
   (org-roam-stack--open "~/documents/roam/elisp.org")
 
   (org-roam-stack--balance-stack))
+
+;;;###autoload
+(define-minor-mode org-roam-stack-mode
+  "Minor mode to organize org roam cards in a stack"
+  :lighter " ors"
+  :global t
+  (if org-roam-stack-mode
+      (progn
+        (org-roam-stack--register-open-file-protocol)
+        (add-hook 'buffer-list-update-hook #'org-roam-stack--buffer-change-hook)
+        (when (functionp 'notdeft-find-file)
+          (advice-add 'notdeft-find-file :around 'org-roam-stack--notdeft-find-file-advice))
+        ;; make sure that starting the org roam server does not place another :roam-file entry!
+        (when (functionp 'org-roam-server-mode)
+          (advice-add 'org-roam-server-mode :after 'org-roam-stack--register-open-file-protocol-advice)))
+
+    (org-roam-stack--unregister-open-file-protocol)
+    (remove-hook 'buffer-list-update-hook #'org-roam-stack--buffer-change-hook)
+    (when (functionp 'notdeft-find-file)
+      (advice-remove 'notdeft-find-file 'org-roam-stack--notdeft-find-file-advice))))
+
+(provide 'org-roam-stack)
